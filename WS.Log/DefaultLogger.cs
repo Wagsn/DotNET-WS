2172d79411664362c79cc547af245bd17f1669ad
wangsen <wagsn@foxmail.com>
2018-11-27 21:37:58 +0800

    更新

---
+++ WS.Log/DefaultLogger.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WS.IO;
+
+namespace WS.Log
+{
+    /// <summary>
+    /// 日志器，每个日志器一个配置，还有一个总配置
+    /// </summary>
+    class DefaultLogger : ILogger
+    {
+        /// <summary>
+        /// 配置文件
+        /// </summary>
+        public LoggerConfig Config { get; set; }
+        
+        /// <summary>
+        /// 配置文件包含日志器名以及文件位置和日志格式等内容
+        /// </summary>
+        /// <param name="config">日志器配置</param>
+        public DefaultLogger(LoggerConfig config)
+        {
+            Config = config;
+        }
+
+        public void Debug(string message)
+        {
+            Log(Config, LogLevels.Debug, message);
+        }
+
+        public void Debug(string formatString, params object[] args)
+        {
+            Log(Config, LogLevels.Debug, formatString, args);
+        }
+
+        public void Error(string message)
+        {
+            Log(Config, LogLevels.Error, message);
+        }
+
+        public void Error(string formatString, params object[] args)
+        {
+
+            Log(Config, LogLevels.Error, formatString, args);
+        }
+
+        public void Fatal(string message)
+        {
+            Log(Config, LogLevels.Fatal, message);
+        }
+
+        public void Fatal(string formatString, params object[] args)
+        {
+            Log(Config, LogLevels.Fatal, formatString, args);
+        }
+
+        public void Info(string message)
+        {
+            Log(Config, LogLevels.Info, message);
+        }
+
+        public void Info(string formatString, params object[] args)
+        {
+            Log(Config, LogLevels.Info, formatString, args);
+        }
+
+        public void Log(LogLevels logLevel, string message)
+        {
+            Log(Config, logLevel, message);
+        }
+
+        public void Log(LogLevels logLevel, string formatString, params object[] args)
+        {
+            Log(Config, logLevel, formatString, args);
+        }
+
+        public void Trace(string message)
+        {
+            Log(Config, LogLevels.Trace, message);
+        }
+
+        public void Trace(string formatString, params object[] args)
+        {
+            Log(Config, LogLevels.Trace, formatString, args);
+        }
+
+        public void Warn(string message)
+        {
+            Log(Config, LogLevels.Warn, message);
+        }
+
+        public void Warn(string formatString, params object[] args)
+        {
+            Log(Config, LogLevels.Warn, formatString, args);
+        }
+
+        /// <summary>
+        /// 带格式化的
+        /// </summary>
+        /// <param name="config">日志器配置文件</param>
+        /// <param name="level">日志层级</param>
+        /// <param name="formatString">模板字符串</param>
+        /// <param name="args">填充字符串数组</param>
+        public static void Log(LoggerConfig config, LogLevels level, string formatString, params object[] args)
+        {
+            Log(config, level, string.Format(formatString, args));
+        }
+
+        /// <summary>
+        /// 不带格式化的
+        /// </summary>
+        /// <param name="config">日志器配置文件</param>
+        /// <param name="level">日志层级</param>
+        /// <param name="message">日志正文</param>
+        public static void Log(LoggerConfig config, LogLevels level, string message)
+        {
+            Log(config, new LogEntity
+            {
+                LogLevel = level,
+                LoggerName = config.LoggerName,
+                LogTime = DateTime.Now,
+                Message = message
+            });
+        }
+
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="config">记录配置（文件保存路径）</param>
+        /// <param name="entity">记录实体（记录包含信息）</param>
+        public static void Log(LoggerConfig config,  LogEntity entity)
+        {
+            //string consoleInfo = $"[{entity.LogLevel.ToString()}] [{entity.LogName}] {entity.Message}";
+            string logItem = $"[{entity.LogTime.ToString(config.TimeFormat)}] [{entity.LogLevel.ToString()}] [{entity.LoggerName}] {entity.Message}";
+            Console.WriteLine(logItem);
+            File.WriteAllText(config.LoggerRoot + "/" + DateTime.Now.ToString(config.FileFormat) + ".log", logItem+"\r\n", true);
+        }
+    }
+}
---
+++ WS.Log/LoggerConfig.cs
@@ -0,0 +1,52 @@
+﻿#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+* 项目名称 ：WS.Core.Log
+* 项目描述 ：
+* 类 名 称 ：LoggerConfig
+* 类 描 述 ：
+* 所在的域 ：DESKTOP-KA4M82K
+* 命名空间 ：WS.Core.Log
+* 机器名称 ：DESKTOP-KA4M82K 
+* CLR 版本 ：4.0.30319.42000
+* 作    者 ：wagsn
+* 创建时间 ：2018/11/23 9:14:28
+* 更新时间 ：2018/11/23 9:14:28
+* 版 本 号 ：v1.0.0.0
+//----------------------------------------------------------------*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WS.Log
+{
+    /// <summary>
+    /// 单独一个日志器的配置
+    /// LoogerName
+    /// LoggerRoot
+    /// FileFormat
+    /// TimeFormat
+    /// </summary>
+    public class LoggerConfig
+    {
+        /// <summary>
+        /// 日志器名称
+        /// </summary>
+        public string LoggerName { get; set; }
+        
+        /// <summary>
+        /// 日志文件根路径（./log/loggerName）
+        /// </summary>
+        public string LoggerRoot { get; set; }
+
+        /// <summary>
+        /// 文件名格式（yyyy-MM-dd）
+        /// </summary>
+        public string FileFormat { get; set; }
+
+        /// <summary>
+        /// 时间格式（2018-11-23 09:53:12.154451+8:00）
+        /// </summary>
+        public string TimeFormat { get; set; }
+    }
+}
