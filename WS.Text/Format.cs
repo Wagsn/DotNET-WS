3998dc816d3b817bedf789f350857596667fb10e
wangsen <wagsn@foxmail.com>
2018-11-27 22:37:43 +0800

    模板字符串替换占位符实现，TODO 优化执行速度

--- WS.Text/Format.cs
+++ WS.Text/Format.cs
@@ -59,6 +59,17 @@ namespace WS.Text
             return false;
         }
 
+        /// <summary>
+        /// 测试模板替换，vs2017->鼠标方法名->右键交互执行->需要把那些依赖的方法和using都交互执行过才行
+        /// </summary>
+        public static void Test()
+        {
+            SafeMap<object> map = new SafeMap<object>();
+            map["username"] = "wagsn";
+            map["password"] = "123456";
+
+            Console.WriteLine(ReplacePlaceholder("${username}: ${password}", map));
+        }
 
 
         /// <summary>
@@ -66,16 +77,135 @@ namespace WS.Text
         /// </summary>
         /// <param name="template"></param>
         /// <returns></returns>
-        public static string ReplacePlaceholder(string template, Dictionary<string, object> pairs)
+        public static string ReplacePlaceholder(string template, SafeMap<object> pairs)
         {
-            // 截取索引及其占位符
-            //Regex regex = new Regex(@"\$\{\S*\}");
-            //Match match = regex.Match(template);
-            //string value = match.Value;
-            // ${username}
-            // 替换
-            return "";
+            string result = new string(template.ToCharArray());
+
+            // 需要优化为，匹配到 \$\{\S*?\} 后按照匹配到的内容作为Key在Map中寻找Value替换
+            foreach(var key in pairs.Keys)
+            {
+                Regex regex = new Regex(@"\$\{"+key+@"\}");
+                result =regex.Replace(result, pairs[key].ToString());
+
+            }
+            return result;
         }
+
+        /// <summary>
+        /// 会自动替换 变量   把形如 "{{varName}}" 替换成对应的数值
+        /// </summary>
+        private static string ReplaceStringVar(string str)
+        {
+            Regex reg = new Regex(@"\{\{(.*?)\}\}");
+            //var mat = reg.Matches(webcofnigstring2);
+
+            str = reg.Replace(str,
+                new MatchEvaluator(m =>
+                     InstallContext.Get(m.Groups[1].Value) == string.Empty ? m.Value : InstallContext.Get(m.Groups[1].Value)
+                ));
+            return str;
+        }
+    }
+
+    /// <summary>
+    /// 安全的映射表，不存在的Key返回默认的Value，string返回string.Empty
+    /// </summary>
+    public class SafeMap<TValue>
+    {
+        /// <summary>
+        /// 获取Keys
+        /// </summary>
+        public IEnumerable<string> Keys { get
+            {
+                return kvs.Keys;
+            } }
+
+        private Dictionary<string, TValue> kvs = new Dictionary<string, TValue>();
+        
+        /// <summary>
+        /// 索引器
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public TValue this[string index]
+        {
+            get
+            {
+                return Get(index);
+            }
+            set
+            {
+                Set(index, value);
+            }
+        }
+
+        /// <summary>
+        /// 获取Key对应的值
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public TValue Get(string index)
+        {
+            if (kvs.ContainsKey(index))
+            {
+                return kvs[index];
+            }
+            else
+            {
+                return default(TValue);
+            }
+        }
+        /// <summary>
+        /// 设置Key对应的值
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="value"></param>
+        public void Set(string index, TValue value)
+        {
+            kvs[index] = value;
+        }
+    }
+
+    /// <summary>
+    /// 键值对
+    /// </summary>
+    public static class InstallContext
+    {
+
+        private static Dictionary<string, string> kvs = new Dictionary<string, string>();
+
+
+        public static string Get(string index)
+        {
+        kvs.Keys
+            if (kvs.ContainsKey(index))
+            {
+                return kvs[index];
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+        public static void Set(string index, string value)
+        {
+            kvs[index] = value;
+        }
+
+
+
+        //private static InstallContext instance = new InstallContext();
+
+        //private InstallContext()
+        //{
+
+        //}
+
+        //public static InstallContext GetInstance()
+        //{ 
+        //    return instance;
+        //}
+
     }
 }
 
