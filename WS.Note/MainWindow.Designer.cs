90ab6b8df1d5dd8db78e633ee9f9e2033f1df364
wangsen <wagsn@foxmail.com>
2018-11-29 21:39:35 +0800

    笔记本

---
+++ WS.Note/MainWindow.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace WS.Note
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.文件 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.新建ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.菜单 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新建ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.关闭ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.另存为ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.视图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.状态栏ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.工具栏ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.状态栏标签 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.当前状态 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.MainToolStrip = new System.Windows.Forms.ToolStrip();
+            this.EditForm = new System.Windows.Forms.TabPage();
+            this.MainTabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.文件.SuspendLayout();
+            this.菜单.SuspendLayout();
+            this.MainStatusStrip.SuspendLayout();
+            this.MainToolStrip.SuspendLayout();
+            this.MainTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // 文件
+            // 
+            this.文件.AllowDrop = true;
+            this.文件.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新建ToolStripMenuItem,
+            this.打开ToolStripMenuItem,
+            this.保存ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.文件.Name = "文件";
+            this.文件.Size = new System.Drawing.Size(101, 92);
+            this.文件.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // 新建ToolStripMenuItem
+            // 
+            this.新建ToolStripMenuItem.Name = "新建ToolStripMenuItem";
+            this.新建ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.新建ToolStripMenuItem.Text = "新建";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.打开ToolStripMenuItem.Text = "打开";
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.保存ToolStripMenuItem.Text = "保存";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            // 
+            // 菜单
+            // 
+            this.菜单.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.编辑ToolStripMenuItem,
+            this.视图ToolStripMenuItem});
+            this.菜单.Location = new System.Drawing.Point(0, 0);
+            this.菜单.Name = "菜单";
+            this.菜单.Size = new System.Drawing.Size(1014, 25);
+            this.菜单.TabIndex = 1;
+            this.菜单.Text = "菜单";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新建ToolStripMenuItem1,
+            this.打开ToolStripMenuItem1,
+            this.关闭ToolStripMenuItem,
+            this.保存ToolStripMenuItem1,
+            this.另存为ToolStripMenuItem,
+            this.退出ToolStripMenuItem1});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 新建ToolStripMenuItem1
+            // 
+            this.新建ToolStripMenuItem1.Name = "新建ToolStripMenuItem1";
+            this.新建ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.新建ToolStripMenuItem1.Text = "新建";
+            // 
+            // 打开ToolStripMenuItem1
+            // 
+            this.打开ToolStripMenuItem1.Name = "打开ToolStripMenuItem1";
+            this.打开ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.打开ToolStripMenuItem1.Text = "打开";
+            // 
+            // 关闭ToolStripMenuItem
+            // 
+            this.关闭ToolStripMenuItem.Name = "关闭ToolStripMenuItem";
+            this.关闭ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.关闭ToolStripMenuItem.Text = "关闭";
+            // 
+            // 保存ToolStripMenuItem1
+            // 
+            this.保存ToolStripMenuItem1.Name = "保存ToolStripMenuItem1";
+            this.保存ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.保存ToolStripMenuItem1.Text = "保存";
+            // 
+            // 另存为ToolStripMenuItem
+            // 
+            this.另存为ToolStripMenuItem.Name = "另存为ToolStripMenuItem";
+            this.另存为ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.另存为ToolStripMenuItem.Text = "另存为";
+            // 
+            // 退出ToolStripMenuItem1
+            // 
+            this.退出ToolStripMenuItem1.Name = "退出ToolStripMenuItem1";
+            this.退出ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.退出ToolStripMenuItem1.Text = "退出";
+            // 
+            // 编辑ToolStripMenuItem
+            // 
+            this.编辑ToolStripMenuItem.Name = "编辑ToolStripMenuItem";
+            this.编辑ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.编辑ToolStripMenuItem.Text = "编辑";
+            // 
+            // 视图ToolStripMenuItem
+            // 
+            this.视图ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.状态栏ToolStripMenuItem,
+            this.工具栏ToolStripMenuItem});
+            this.视图ToolStripMenuItem.Name = "视图ToolStripMenuItem";
+            this.视图ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.视图ToolStripMenuItem.Text = "视图";
+            // 
+            // 状态栏ToolStripMenuItem
+            // 
+            this.状态栏ToolStripMenuItem.CheckOnClick = true;
+            this.状态栏ToolStripMenuItem.Name = "状态栏ToolStripMenuItem";
+            this.状态栏ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.状态栏ToolStripMenuItem.Text = "状态栏";
+            this.状态栏ToolStripMenuItem.Click += new System.EventHandler(this.状态栏ToolStripMenuItem_Click_1);
+            // 
+            // 工具栏ToolStripMenuItem
+            // 
+            this.工具栏ToolStripMenuItem.CheckOnClick = true;
+            this.工具栏ToolStripMenuItem.Name = "工具栏ToolStripMenuItem";
+            this.工具栏ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.工具栏ToolStripMenuItem.Text = "工具栏";
+            this.工具栏ToolStripMenuItem.Click += new System.EventHandler(this.工具栏ToolStripMenuItem_Click);
+            // 
+            // MainStatusStrip
+            // 
+            this.MainStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.状态栏标签,
+            this.当前状态});
+            this.MainStatusStrip.Location = new System.Drawing.Point(0, 521);
+            this.MainStatusStrip.Name = "MainStatusStrip";
+            this.MainStatusStrip.Size = new System.Drawing.Size(1014, 22);
+            this.MainStatusStrip.TabIndex = 2;
+            this.MainStatusStrip.Text = "状态栏";
+            this.MainStatusStrip.Visible = false;
+            this.MainStatusStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            // 
+            // 状态栏标签
+            // 
+            this.状态栏标签.Name = "状态栏标签";
+            this.状态栏标签.Size = new System.Drawing.Size(44, 17);
+            this.状态栏标签.Text = "状态栏";
+            // 
+            // 当前状态
+            // 
+            this.当前状态.Name = "当前状态";
+            this.当前状态.Size = new System.Drawing.Size(56, 17);
+            this.当前状态.Text = "当前状态";
+            // 
+            // MainToolStrip
+            // 
+            this.MainToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.MainToolStrip.Location = new System.Drawing.Point(0, 25);
+            this.MainToolStrip.Name = "MainToolStrip";
+            this.MainToolStrip.Size = new System.Drawing.Size(1014, 25);
+            this.MainToolStrip.TabIndex = 3;
+            this.MainToolStrip.Text = "工具栏";
+            this.MainToolStrip.Visible = false;
+            // 
+            // EditForm
+            // 
+            this.EditForm.Location = new System.Drawing.Point(4, 22);
+            this.EditForm.Name = "EditForm";
+            this.EditForm.Padding = new System.Windows.Forms.Padding(3);
+            this.EditForm.Size = new System.Drawing.Size(1006, 492);
+            this.EditForm.TabIndex = 0;
+            this.EditForm.Tag = "WS.Note.EditForm";
+            this.EditForm.Text = "EditForm";
+            this.EditForm.UseVisualStyleBackColor = true;
+            // 
+            // MainTabControl
+            // 
+            this.MainTabControl.Controls.Add(this.EditForm);
+            this.MainTabControl.Controls.Add(this.tabPage1);
+            this.MainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainTabControl.Location = new System.Drawing.Point(0, 25);
+            this.MainTabControl.Name = "MainTabControl";
+            this.MainTabControl.SelectedIndex = 0;
+            this.MainTabControl.Size = new System.Drawing.Size(1014, 518);
+            this.MainTabControl.TabIndex = 4;
+            this.MainTabControl.SelectedIndexChanged += new System.EventHandler(this.mainTabControl_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1006, 445);
+            this.tabPage1.TabIndex = 1;
+            this.tabPage1.Tag = "WS.Note.EditForm";
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(52, 22);
+            this.toolStripButton1.Text = "运行";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 543);
+            this.Controls.Add(this.MainTabControl);
+            this.Controls.Add(this.MainToolStrip);
+            this.Controls.Add(this.MainStatusStrip);
+            this.Controls.Add(this.菜单);
+            this.MainMenuStrip = this.菜单;
+            this.Name = "MainWindow";
+            this.Text = "Note";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.文件.ResumeLayout(false);
+            this.菜单.ResumeLayout(false);
+            this.菜单.PerformLayout();
+            this.MainStatusStrip.ResumeLayout(false);
+            this.MainStatusStrip.PerformLayout();
+            this.MainToolStrip.ResumeLayout(false);
+            this.MainToolStrip.PerformLayout();
+            this.MainTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip 文件;
+        private System.Windows.Forms.ToolStripMenuItem 新建ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip 菜单;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 编辑ToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip MainStatusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel 状态栏标签;
+        private System.Windows.Forms.ToolStripMenuItem 新建ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripStatusLabel 当前状态;
+        private System.Windows.Forms.ToolStripMenuItem 关闭ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStrip MainToolStrip;
+        private System.Windows.Forms.TabPage EditForm;
+        private System.Windows.Forms.TabControl MainTabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ToolStripMenuItem 另存为ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 视图ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 状态栏ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 工具栏ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ WS.Note/MainWindow.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace WS.Note
+{
+    public partial class MainWindow : Form
+    {
+        public int[] IsSelectedList = { 0, 0 };         //用来记录from是否打开过  
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 主窗口加载时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            //初始打开时就加载Form2  
+            string formClass = "WS.Note.EditForm";
+            GenerateForm(formClass, MainTabControl);
+        }
+
+        /// <summary>
+        /// 反射生成窗口，利用反射动态的加载窗体到对应的TabPage的。
+        /// </summary>
+        /// <param name="formClassName"></param>
+        /// <param name="sender"></param>
+        public void GenerateForm(string formClassName, object sender)
+        {
+            // 反射生成窗体  
+            Form fm = (Form)Assembly.GetExecutingAssembly().CreateInstance(formClassName);
+
+            //设置窗体没有边框 加入到选项卡中  
+            fm.FormBorderStyle = FormBorderStyle.None;
+            fm.TopLevel = false;
+            fm.Parent = ((TabControl)sender).SelectedTab;
+            fm.ControlBox = false;
+            fm.Dock = DockStyle.Fill;
+            fm.Show();
+
+            IsSelectedList[((TabControl)sender).SelectedIndex] = 1;
+
+        }
+
+        /// <summary>
+        /// 选项卡切换
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mainTabControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //只生成一次
+            if (IsSelectedList[MainTabControl.SelectedIndex] == 0)
+            {
+                btn_Click(sender, e);
+            }
+        }
+
+        /// <summary>  
+        /// 通用按钮点击选项卡 在选项卡上显示对应的窗体  
+        /// </summary>  
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string formClass = ((TabControl)sender).SelectedTab.Tag.ToString();
+            GenerateForm(formClass, sender);
+        }
+
+        /// <summary>
+        /// 菜单-视图-状态栏选则框
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void 状态栏ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 状态栏菜单被点击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void 状态栏ToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            // MainStatusStrip
+            if (!MainStatusStrip.Visible)
+            {
+                MainStatusStrip.Visible = true;
+            }
+            else
+            {
+                MainStatusStrip.Visible = false;
+            }
+        }
+
+        /// <summary>
+        /// 工具栏菜单被点击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void 工具栏ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // MainToolStrip
+            if (!MainToolStrip.Visible)
+            {
+                MainToolStrip.Visible = true;
+            }
+            else
+            {
+                MainToolStrip.Visible = false;
+            }
+        }
+    }
+}
