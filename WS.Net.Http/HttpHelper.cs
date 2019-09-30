9351ffe76eb7b55ec50e8e7662be8fd36982fe9e
Wagsn <wagsn@foxmail.com>
2019-09-30 17:38:17 +0800

    HTTP请求助手

---
+++ WS.Net.Http/HttpHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WS.Net.Http
+{
+    /// <summary>
+    /// HTTP请求助手
+    /// </summary>
+    public class HttpHelper
+    {
+        /// <summary>
+        /// 发送请求
+        /// </summary>
+        /// <typeparam name="TBody">响应体ResponseBody</typeparam>
+        /// <param name="url">URL，可能带Params</param>
+        /// <param name="method">HTTP方法</param>
+        /// <param name="data">携带的数据，可能解析成Params或RequestBody</param>
+        /// <returns></returns>
+        public static TBody Send<TBody>(string url, string method, object data)
+        {
+            return default(TBody);
+        }
+    }
+}
---
+++ WS.Net.Http/HttpMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WS.Net.Http
+{
+    /// <summary>
+    /// HTTP方法
+    /// </summary>
+    public class HttpMethod
+    {
+        public static readonly string GET = nameof(GET);
+        public static readonly string POST = nameof(POST);
+        public static readonly string PUT = nameof(PUT);
+        public static readonly string DELETE = nameof(DELETE);
+        public static readonly string HEAD = nameof(HEAD);
+        public static readonly string OPTIONS = nameof(OPTIONS);
+        public static readonly string PATCH = nameof(PATCH);
+        public static readonly string COPY = nameof(COPY);
+        public static readonly string LINK = nameof(LINK);
+        public static readonly string UNLINK = nameof(UNLINK);
+        public static readonly string PURGE = nameof(PURGE);
+        public static readonly string LOCK = nameof(LOCK);
+        public static readonly string UNLOCK = nameof(UNLOCK);
+        public static readonly string PROPFIND = nameof(PROPFIND);
+        public static readonly string VIEW = nameof(VIEW);
+    }
+}
