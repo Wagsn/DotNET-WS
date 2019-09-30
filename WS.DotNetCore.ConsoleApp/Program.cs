fb4accb06d10021e85845bd97fde838dac9bbb6f
Wagsn <wagsn@foxmail.com>
2019-09-30 17:39:07 +0800

    添加：MailKit方式发送邮件

--- WS.DotNetCore.ConsoleApp/Program.cs
+++ WS.DotNetCore.ConsoleApp/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Net;
-using System.Net.Mail;
 
 namespace WS.DotNetCore.ConsoleApp
 {
@@ -8,33 +6,62 @@ namespace WS.DotNetCore.ConsoleApp
     {
         static void Main(string[] args)
         {
+            // Mailbox Authorization Code: lmiuqpvhlnmebcfe
+
             // MailKit / MimeKit
-            // 邮箱授权码：lmiuqpvhlnmebcfe
-            MailMessage mailMessage = new MailMessage();
-            mailMessage.From = new MailAddress("wagsn@foxmail.com", "Wagsn");
-            mailMessage.To.Add(new MailAddress("vsm2464@dingtalk.com"));
-            //var bodyBuilder = new BodyBuilder();
-            //mailMessage.To.Add(new MailAddress("774284673@qq.com"));
-            mailMessage.Subject = ".NET Core 邮件发送测试";
-            //mailMessage.Body = "纯文本测试第一行\r\n纯文本测试第二行";
-            mailMessage.Body = "<h1>富文本标题</h1><p>富文本正文</p>";
-            mailMessage.IsBodyHtml = true;
+            var message = new MimeKit.MimeMessage();
+            message.From.Add(new MimeKit.MailboxAddress("Wagsn", "wagsn@foxmail.com"));
+            message.To.Add(new MimeKit.MailboxAddress("2464", "vsm2464@dingtalk.com"));
 
-            SmtpClient client = new SmtpClient();
-            client.Host = "smtp.qq.com";
-            client.Port = 587;
-            client.EnableSsl = true;
-            client.Credentials = new NetworkCredential("wagsn@foxmail.com", "lmiuqpvhlnmebcfe");
+            message.Subject = "星期天去哪里玩？";
+            var bodyBuilder = new MimeKit.BodyBuilder();
+            bodyBuilder.HtmlBody = @"<h1>计划</h1><p>我想去故宫玩，如何</p>";
+            message.Body = bodyBuilder.ToMessageBody();
+            //message.Body = new TextPart("计划") { Text = "我想去故宫玩，如何" };
 
-            // 发送
-            try
+            using (var client2 = new MailKit.Net.Smtp.SmtpClient())
             {
-                client.Send(mailMessage);
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                client2.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client2.Connect("smtp.qq.com", 587, false);
+
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client2.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                // Note: only needed if the SMTP server requires authentication
+                client2.Authenticate("wagsn@foxmail.com", "lmiuqpvhlnmebcfe");
+
+                client2.Send(message);
+                client2.Disconnect(true);
             }
-            catch(Exception e)
-            {
-                Console.Error.WriteLine($"Error: {e}");
-            }
+
+            //System.Net.Mail.MailMessage mailMessage = new System.Net.Mail.MailMessage();
+            //mailMessage.From = new System.Net.Mail.MailAddress("wagsn@foxmail.com", "Wagsn");
+            //mailMessage.To.Add(new System.Net.Mail.MailAddress("vsm2464@dingtalk.com"));
+            ////var bodyBuilder = new BodyBuilder();
+            ////mailMessage.To.Add(new MailAddress("774284673@qq.com"));
+            //mailMessage.Subject = ".NET Core 邮件发送测试";
+            ////mailMessage.Body = "纯文本测试第一行\r\n纯文本测试第二行";
+            //mailMessage.Body = "<h1>富文本标题</h1><p>富文本正文</p>";
+            //mailMessage.IsBodyHtml = true;
+
+            //System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();
+            //client.Host = "smtp.qq.com";
+            //client.Port = 587;
+            //client.EnableSsl = true;
+            //client.Credentials = new NetworkCredential("wagsn@foxmail.com", "lmiuqpvhlnmebcfe");
+
+            //try
+            //{
+            //    client.Send(mailMessage);
+            //}
+            //catch(Exception e)
+            //{
+            //    Console.Error.WriteLine($"Error: {e}");
+            //}
+
             Console.WriteLine("邮件已发送");
             Console.ReadKey();
         }
